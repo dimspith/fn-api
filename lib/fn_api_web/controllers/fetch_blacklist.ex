b48a50837cb308fb2c0e20c7bbc0761237d27d72
dimspith <51262483+dimspith@users.noreply.github.com>
2021-11-05 10:21:19 +0200

    Serve list as a json file, add bloom filter dependencies

--- lib/fn_api_web/controllers/fetch_blacklist.ex
+++ lib/fn_api_web/controllers/fetch_blacklist.ex
@@ -1,11 +1,12 @@
 defmodule FnApiWeb.FetchBlacklist do
   use FnApiWeb, :controller
+  
+  File.write!("priv/top1m.json", Jason.encode! %{"sites" => File.read!("priv/top1m.bcp")
+                                            |> String.split("\n", trim: true)
+                                            |> Enum.map(fn x -> "*://*." <> x <> "/*" end)})
 
-  @blacklist %{"sites" => ["*://*.google.com/*", "*://*.youtube.com/*"]}
 
   def index(conn, _params) do
-    conn
-    |> put_resp_content_type("application/json")
-    |> send_resp(200, Jason.encode!(@blacklist))
+    send_file(conn, 200, "top1m.json")
   end
 end
--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule FnApi.MixProject do
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:talan, "~> 0.1.0"}
     ]
   end
 
