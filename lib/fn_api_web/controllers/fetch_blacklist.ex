bebfe364c7505012e27efaa8aa8e6b20c27a3ccc
dimspith <51262483+dimspith@users.noreply.github.com>
2021-11-05 14:13:03 +0200

    Change list name

--- lib/fn_api_web/controllers/fetch_blacklist.ex
+++ lib/fn_api_web/controllers/fetch_blacklist.ex
@@ -1,12 +1,11 @@
 defmodule FnApiWeb.FetchBlacklist do
   use FnApiWeb, :controller
   
-  File.write!("priv/top1m.json", Jason.encode! %{"sites" => File.read!("priv/top1m.bcp")
+  File.write("priv/list.json", Jason.encode! %{"sites" => File.read!("priv/list")
                                             |> String.split("\n", trim: true)
                                             |> Enum.map(fn x -> "*://*." <> x <> "/*" end)})
-
-
+  
   def index(conn, _params) do
-    send_file(conn, 200, "top1m.json")
+    send_file(conn, 200, "priv/list.json")
   end
 end
