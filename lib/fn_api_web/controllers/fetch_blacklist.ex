89fd2a25d85b64096d45facab085ab2ca2c0060c
dimspith <51262483+dimspith@users.noreply.github.com>
2022-02-03 20:23:14 +0200

    Change list modification

--- lib/fn_api_web/controllers/fetch_blacklist.ex
+++ lib/fn_api_web/controllers/fetch_blacklist.ex
@@ -1,9 +1,14 @@
 defmodule FnApiWeb.FetchBlacklist do
   use FnApiWeb, :controller
+
+  # Reload module when lists change
+  @external_resource "priv/list"
+  @external_resource "priv/list.json"
   
-  File.write("priv/list.json", Jason.encode! %{"sites" => File.read!("priv/list")
-                                            |> String.split("\n", trim: true)
-                                            |> Enum.map(fn x -> "*://*." <> x <> "/*" end)})
+  # Convert plaintext list to json file
+  File.write("priv/list.json",
+    Jason.encode! %{"sites" => File.read!("priv/list")
+                    |> String.split("\n", trim: true)})
   
   def index(conn, _params) do
     send_file(conn, 200, "priv/list.json")
