7d5a436db6e55cdad0ff9c1a406ed52fbc8319fe
dimspith <51262483+dimspith@users.noreply.github.com>
2022-03-02 20:43:57 +0200

    Sort lists before serving to client

--- lib/fn_api_web/controllers/fetch_blacklist.ex
+++ lib/fn_api_web/controllers/fetch_blacklist.ex
@@ -4,12 +4,15 @@ defmodule FnApiWeb.FetchBlacklist do
   # Reload module when lists change
   @external_resource "priv/list"
   @external_resource "priv/list.json"
-  
+
   # Convert plaintext list to json file
-  File.write("priv/list.json",
-    Jason.encode! %{"sites" => File.read!("priv/list")
-                    |> String.split("\n", trim: true)})
-  
+  list =
+    File.read!("priv/list")
+    |> String.split("\n", trim: true)
+    |> Enum.sort()
+
+  File.write("priv/list.json", Jason.encode!(%{"sites" => list}))
+
   def index(conn, _params) do
     send_file(conn, 200, "priv/list.json")
   end
