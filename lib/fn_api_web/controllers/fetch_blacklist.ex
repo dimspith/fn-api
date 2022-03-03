68c8e91e04ed81af1416e67cc3793ebeb12d6546
dimspith <51262483+dimspith@users.noreply.github.com>
2022-03-03 18:59:25 +0200

    Remove list watchers

--- lib/fn_api_web/controllers/fetch_blacklist.ex
+++ lib/fn_api_web/controllers/fetch_blacklist.ex
@@ -4,10 +4,6 @@ defmodule FnApiWeb.FetchBlacklist do
   alias FnApi.Repo
   alias FnApi.Blacklist
 
-  # Reload module when lists change
-  @external_resource "priv/lists/list"
-  @external_resource "priv/lists/list.json"
-
   def index(conn, _params) do
     send_file(conn, 200, "priv/lists/list.json")
     json(conn, Jason.encode!(%{"sites" => Repo.all(from(i in Blacklist, select: i.domain))}))
