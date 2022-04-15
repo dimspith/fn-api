2c3fee1327436a77d0508630f0d8b5b9702da8fa
dimspith <51262483+dimspith@users.noreply.github.com>
2022-04-15 15:13:21 +0300

    Tweak helper function names

--- .iex.exs
+++ .iex.exs
@@ -2,12 +2,12 @@ import Ecto.Query
 alias FnApi.{Repo, Insertions, Deletions, Checkpoints, Labels, Tokens, Tags}
 
 defmodule IU do
-  def iex_tbn(fullName) do
+  def get_uuid(fullName) do
     Ecto.UUID.load! Repo.get_by(Tokens, fullName: fullName).uuid
   end
   def insert_token(name) do
     Repo.insert(%Tokens{fullName: name})
-    iex_tbn(name)
+    get_uuid(name)
   end
 end
 
