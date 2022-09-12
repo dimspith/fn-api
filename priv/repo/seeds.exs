784418aaa594b295707dcff06381a5d03a4f572b
dimspith <51262483+dimspith@users.noreply.github.com>
2022-09-12 14:20:53 +0300

    Optimize seeds script

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -4,17 +4,28 @@
 #
 #     mix run priv/repo/seeds.exs
 #
-
 alias FnApi.Database.{Repo, Insertions, Checkpoints}
+
+# Options
+chunk_size = 500
+
 
 curr_datetime = DateTime.now!("Etc/UTC") |> DateTime.to_unix()
 
-File.stream!(Application.app_dir(:fn_api, "priv") <> "/lists/default")|> Enum.each(fn domain ->
-  domain
-  |> String.trim()
-  |> (fn domain ->
-    Repo.insert!(%Insertions{domain: domain, date: curr_datetime})
-  end).()
+# Get default list file
+File.stream!(Application.app_dir(:fn_api, "priv") <> "/lists/default")
+# Chunk file every 500 lines
+|> Stream.chunk_every(chunk_size)
+|> Stream.map(fn list ->
+  Enum.map(list, fn domain ->
+    %{domain: String.trim(domain), date: curr_datetime}
+  end)
+end)
+|> Enum.map(fn domains ->
+  Ecto.Multi.new()
+  |> Ecto.Multi.insert_all(:insert_all, Insertions, domains)
+  |> Repo.transaction()
 end)
 
 Repo.insert(%Checkpoints{date: curr_datetime})
+
