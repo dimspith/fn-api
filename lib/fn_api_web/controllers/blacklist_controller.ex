5dac358aeb8caf256954d57ba3ce46ef949b37d2
dimspith <51262483+dimspith@users.noreply.github.com>
2022-10-18 14:08:38 +0300

    Rename lastupdate to checkpoint in blacklist controller

--- lib/fn_api_web/controllers/blacklist_controller.ex
+++ lib/fn_api_web/controllers/blacklist_controller.ex
@@ -4,7 +4,7 @@ defmodule FnApiWeb.BlacklistController do
 
   def fetch_blacklist(conn, params) do
     case params do
-      %{"lastupdate" => unix_time} -> send_diffs(conn, unix_time)
+      %{"checkpoint" => unix_time} -> send_diffs(conn, unix_time)
       _ -> send_all(conn)
     end
   end
@@ -14,7 +14,7 @@ defmodule FnApiWeb.BlacklistController do
         File.read!(blacklist_file())
         |> String.split("\n")
         |> (fn list -> List.delete_at(list, length(list) - 1) end).(),
-      "lastupdate" => get_last_update()
+      "checkpoint" => get_last_update()
     })
   end
 
@@ -23,7 +23,7 @@ defmodule FnApiWeb.BlacklistController do
       {date, _} ->
         diff = generate_diff(date)
         json(conn, %{
-          "lastupdate" => get_last_update(),
+          "checkpoint" => get_last_update(),
           "insertions" => diff[:insert],
           "deletions" => diff[:delete]
         }) 
